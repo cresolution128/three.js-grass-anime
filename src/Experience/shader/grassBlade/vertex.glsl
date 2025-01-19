924e6e133860bd41f53e5dbc7d4d8b118badd810
CResolution <cresolution0128@gmail.com>
2025-01-20 00:20:13 +0530

    minor change for wind

--- src/Experience/shader/grassBlade/vertex.glsl
+++ src/Experience/shader/grassBlade/vertex.glsl
@@ -16,6 +16,7 @@ void main() {
     float curveStrength = simplexNoise2d(vec2( sin(uTime *2.0)*0.05, cos(uTime * 1.0) * 0.05)) ; // Control the maximum bending
     float heightFactor = position.y * 0.075; // Scales the bending effect (stronger at top, weaker at bottom)
     transform.x += (position.y * 0.25) * curveStrength * heightFactor ;
+    transform.z += (position.y * 0.15) * curveStrength * heightFactor ;
     // Output the transformed position
     csm_Position = transform;
 
